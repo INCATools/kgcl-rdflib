eb0af12c25486642640cc8f3c67ee9832ebeae6a
chris <chris.kind.man@gmail.com>
2021-10-13 10:47:48 +0100

    Adapt tests wrt to new semantics for 'create/delete edge'

--- kgcl/tests/test_apply/test_delete_edge.py
+++ kgcl/tests/test_apply/test_delete_edge.py
@@ -2,7 +2,10 @@ from kgcl.tests.test_apply.util import run_test
 
 
 def test_delete_edge_with_ids():
-    input_graph = "<http://example.org/subject> <http://example.org/predicate> <http://example.org/object> ."
+    input_graph = """_:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#onProperty> <http://example.org/predicate> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#someValuesFrom> <http://example.org/object> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Restriction> .
+                     <http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#subClassOf> _:Ndb451c3aad3841c88d7042f29b20bea8 ."""
     kgcl_patch = "delete edge <http://example.org/subject> <http://example.org/predicate> <http://example.org/object>"
     expected_graph = ""
 
@@ -10,7 +13,10 @@ def test_delete_edge_with_ids():
 
 
 def test_delete_edge_with_curies():
-    input_graph = "<http://example.org/subject> <http://example.org/predicate> <http://example.org/object> ."
+    input_graph = """_:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#onProperty> <http://example.org/predicate> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#someValuesFrom> <http://example.org/object> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Restriction> .
+                     <http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#subClassOf> _:Ndb451c3aad3841c88d7042f29b20bea8 ."""
     kgcl_patch = "delete edge ex:subject ex:predicate ex:object"
     expected_graph = ""
 
@@ -18,8 +24,32 @@ def test_delete_edge_with_curies():
 
 
 def test_delete_edge_with_labels():
-    input_graph = """<http://example.org/subject> <http://example.org/predicate> <http://example.org/object> .
-                     <http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#label> "subject" ."""
-    kgcl_patch = "delete edge 'subject' ex:predicate ex:object"
-    expected_graph = '<http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#label> "subject" .'
+    input_graph = """_:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#onProperty> <http://example.org/predicate> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/2002/07/owl#someValuesFrom> <http://example.org/object> .
+                     _:Ndb451c3aad3841c88d7042f29b20bea8 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Restriction> .
+                     <http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#subClassOf> _:Ndb451c3aad3841c88d7042f29b20bea8 .
+                     <http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#label> "subject" .
+                     <http://example.org/predicate> <http://www.w3.org/2000/01/rdf-schema#label> "predicate" .
+                     <http://example.org/object> <http://www.w3.org/2000/01/rdf-schema#label> "object" ."""
+    kgcl_patch = "delete edge 'subject' 'predicate' 'object'"
+    expected_graph = """<http://example.org/subject> <http://www.w3.org/2000/01/rdf-schema#label> "subject" .
+                        <http://example.org/predicate> <http://www.w3.org/2000/01/rdf-schema#label> "predicate" .
+                        <http://example.org/object> <http://www.w3.org/2000/01/rdf-schema#label> "object" ."""
+
+    run_test(input_graph, kgcl_patch, expected_graph)
+
+
+def test_delete_subsumption_with_ids():
+    input_graph = "<http://example.org/subclass> <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://example.org/superclass> ."
+    kgcl_patch = "delete edge <http://example.org/subclass> <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://example.org/superclass>"
+    expected_graph = ""
+
+    run_test(input_graph, kgcl_patch, expected_graph)
+
+
+def test_delete_subsumption_with_curies():
+    input_graph = "<http://example.org/subclass> <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://example.org/superclass> ."
+    kgcl_patch = "delete edge ex:subclass rdfs:subClassOf ex:superclass"
+    expected_graph = ""
+
     run_test(input_graph, kgcl_patch, expected_graph)
